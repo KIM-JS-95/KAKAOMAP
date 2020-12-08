b0e932da39994bf84465c0ed1158a8a9459d329d
KIM-JS-95 <baugh248730@gmail.com>
2020-12-08 20:06:02 +0900

    MySql conn 완료

--- src/main/java/com/kakaomap/web/IndexController.java
+++ src/main/java/com/kakaomap/web/IndexController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
 
     @GetMapping("/")
+    //View.mustache 파일을 불러와 화면에 표시
     public String View(){
         return "View";
     }
+
 }
